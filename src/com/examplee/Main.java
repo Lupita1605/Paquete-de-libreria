a6d695fce872df33875ec43c7a53b920953be565
Lupita1605 <guadalupevelazquezlopez46@gmail.com>
2025-03-21 23:45:22 -0600

    Paquete de libreria

---
+++ src/com/examplee/Main.java
@@ -0,0 +1,12 @@
+
+package com.examplee;
+
+import com.example.gui.UserManagementGUI;
+
+public class Main {
+    public static void main(String[] args) {
+        new UserManagementGUI();
+    }
+}
+    
+
